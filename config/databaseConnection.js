cc127ec6df823394826d83cdfba102c42a6a9369
BeliusY <noob.h18.y@gmail.com>
2022-08-23 14:16:57 +0400

    try catch in db connection

--- config/databaseConnection.js
+++ config/databaseConnection.js
@@ -1,5 +1,9 @@
 import mongoose from 'mongoose';
 
 export default function databaseConnection() {
-  mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/RPG_LIFE');
+  try {
+    mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/RPG_LIFE');
+  } catch (error) {
+    console.log('errorishe', error)
+  }
 }
